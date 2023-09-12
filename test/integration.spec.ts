c74b5c8b1e38ff302519237414c0f138ac08aa15
Lukas Wölfer <lukas.woelfer@rwth-aachen.de>
2023-09-12 14:59:00 +0200

    Improved test

--- test/integration.spec.ts
+++ test/integration.spec.ts
@@ -9,10 +9,16 @@ const CLI_FILE = "dist/index.mjs";
 describe("Integration", () => {
   it("Serializes", async () => {
     const input = "hey";
-    const splitResult = execPromise(`node ${CLI_FILE} -r 5`);
+    const requiredCount = 5;
+    const extraCount = 5;
+    const splitResult = execPromise(
+      `node ${CLI_FILE} -r ${requiredCount} -e ${extraCount}`
+    );
     splitResult.child.stdin?.write(input);
     splitResult.child.stdin?.end();
     const { stdout, stderr } = await splitResult;
+    assert.equal(stdout.trim().split("\n").length, requiredCount + extraCount);
+
     const mergeResult = execPromise(`node ${CLI_FILE} merge`);
     mergeResult.child.stdin?.write(stdout);
     mergeResult.child.stdin?.end();
